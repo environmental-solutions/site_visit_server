818184aec5e1aaa485900160a288db5589d9213d
Daniel Dosen <dgdosen@gmail.com>
2017-11-09 17:53:31 -0800

    recommended devise setup (missing home controller)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+
+  devise_for :users
   post "/graphql", to: "graphql#execute"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'hello_world', to: 'hello_world#index'
+  root to: "home#index"
 
 end
