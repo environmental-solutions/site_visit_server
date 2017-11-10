1f5f659850d9d1367bfe7caa8c78b44e44fddaf4
Daniel Dosen <dgdosen@gmail.com>
2017-11-10 12:04:08 -0800

    adding sample data for graphql

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#
+
+project = Project.where({name: 'Project 1'}).first_or_create
+
+site1 = Site.where({name: 'Site 1', description: 'Site 1 description'}).first_or_create
+site2 = Site.where({name: 'Site 2', description: 'Site 2 description'}).first_or_create
+
+project.sites << site1
+project.sites << site2
+
+
