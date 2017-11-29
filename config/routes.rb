2631aa96e3c3bf41deb523b33dfd95d6936963c0
Daniel Dosen <dgdosen@gmail.com>
2017-11-29 12:09:14 -0800

    ui-ifying app to allow for graphiql to run over server api

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  if Rails.env.development?
+    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
+  end
+
   devise_for :users
   post "/graphql", to: "graphql#execute"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
