43bb5599085ac5e7a03c8a70349903724d28ab7b
Daniel Dosen <dgdosen@gmail.com>
2017-11-29 11:36:26 -0800

    wip first mutaiton

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -8,4 +8,27 @@ Types::MutationType = GraphQL::ObjectType.define do
       "Hello World!"
     }
   end
+
+  field :createProject, !types[Types::ProjectType] do
+    description "Create a project with a name and description"
+    argument :project, ProjectInputType
+    resolve ->(t, args, c) {
+      puts "called create_project on server"
+      Project.create!(args[:project])
+    }
+  end
+
+end
+
+ProjectInputType = GraphQL::InputObjectType.define do
+  name "ProjectInputType"
+  description "Properties for creating a Project"
+
+  argument :name, !types.String do
+    description "Name of the project."
+  end
+
+  argument :description, types.String do
+    description "Description of the project."
+  end
 end
