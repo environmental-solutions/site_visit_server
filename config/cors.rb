d0e9014f95740c4b2f465b2ee97a1a82690257b7
Daniel Dosen <dgdosen@gmail.com>
2017-12-01 09:00:03 -0800

    using environment info in cors.rb

--- config/cors.rb
+++ config/cors.rb
@@ -2,7 +2,7 @@ Rails.application.config.middleware.insert_before 0, "Rack::Cors" do
  allow do
    # fixme: what's minimally required here?
    # origins 'localhost:4200', 'beta.sessuru.com', 'http://app.beta.sessuru.com.s3-website-us-west-2.amazonaws.com/sessuru'
-   origins '*'
+   origins ENV['CORS_ORIGIN']
 
    resource '*',
      headers: :any,
