aba5226b420d62d1f0cc031e98a53e45cab6f134
Daniel Dosen <dgdosen@gmail.com>
2017-12-04 19:07:41 -0800

    refactoring active record properties

--- app/graphql/types/project_type.rb
+++ app/graphql/types/project_type.rb
@@ -2,7 +2,7 @@ Types::ProjectType = GraphQL::ObjectType.define do
 
   name 'Project'
 
-  field :id, !types.ID
+  interfaces [Types::ActiveRecordInterface]
   field :name, !types.String
   field :sites, types[Types::SiteType]
 
