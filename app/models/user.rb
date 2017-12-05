eb3daaea6f31321363f7e19e08d8c02e810fca07
Daniel Dosen <dgdosen@gmail.com>
2017-12-05 15:31:29 -0800

    wip: ensuring users have a token

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,22 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_secure_token :token
+  before_create :set_access_token
+  has_secure_token
+
+  validates :email, presence: true, uniqueness: true
+
+  private
+
+  def set_access_token
+    self.token = generate_token
+  end
+
+  def generate_token
+    loop do
+      token = SecureRandom.hex(10)
+      break token unless User.where(token: token).exists?
+    end
+  end
 
 end
