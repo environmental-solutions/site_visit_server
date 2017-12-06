ca0d5d42fae8f8e9e73c141362eb58c735a49704
Daniel Dosen <dgdosen@gmail.com>
2017-12-06 06:19:46 -0800

    refactored code to has_secure_token

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,22 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  before_create :set_access_token
   has_secure_token
 
   validates :email, presence: true, uniqueness: true
 
   private
-
-  def set_access_token
-    self.token = generate_token
-  end
-
-  def generate_token
-    loop do
-      token = SecureRandom.hex(10)
-      break token unless User.where(token: token).exists?
-    end
-  end
 
 end
